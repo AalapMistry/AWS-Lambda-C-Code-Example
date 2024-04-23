1fe3d077ee78ae97f910520e71b620ad30f9198e
AalapMistry <mistryaalap@gmail.com>
2024-04-23 20:24:01 +0530

    ItemController.cs

--- ItemController.cs
+++ ItemController.cs
@@ -1,9 +1,4 @@
-public class Item
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-}
+
 
 public class ItemController : Controller
 {
