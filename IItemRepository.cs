2a4f121ea9f74db79cb2360e3e4689bd34013ab3
AalapMistry <mistryaalap@gmail.com>
2024-04-23 20:07:43 +0530

    IItemRepository.cs

--- IItemRepository.cs
+++ IItemRepository.cs
@@ -9,32 +9,72 @@ public class ItemRepository : IItemRepository
 
     public async Task<IEnumerable<Item>> GetAllItems()
     {
-        return await _context.Items.ToListAsync();
+        try
+        {
+            return await _context.Items.ToListAsync();
+        }
+        catch (Exception ex)
+        {
+            // Handle exception
+            throw new Exception("Error retrieving items", ex);
+        }
     }
 
     public async Task<Item> GetItemById(int id)
     {
-        return await _context.Items.FindAsync(id);
+        try
+        {
+            return await _context.Items.FindAsync(id);
+        }
+        catch (Exception ex)
+        {
+            // Handle exception
+            throw new Exception("Error retrieving item by ID", ex);
+        }
     }
 
     public async Task<Item> CreateItem(Item item)
     {
-        _context.Items.Add(item);
-        await _context.SaveChangesAsync();
-        return item;
+        try
+        {
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+            return item;
+        }
+        catch (Exception ex)
+        {
+            // Handle exception
+            throw new Exception("Error creating item", ex);
+        }
     }
 
     public async Task<Item> UpdateItem(Item item)
     {
-        _context.Entry(item).State = EntityState.Modified;
-        await _context.SaveChangesAsync();
-        return item;
+        try
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return item;
+        }
+        catch (Exception ex)
+        {
+            // Handle exception
+            throw new Exception("Error updating item", ex);
+        }
     }
 
     public async Task DeleteItem(int id)
     {
-        var item = await _context.Items.FindAsync(id);
-        _context.Items.Remove(item);
-        await _context.SaveChangesAsync();
+        try
+        {
+            var item = await _context.Items.FindAsync(id);
+            _context.Items.Remove(item);
+            await _context.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            // Handle exception
+            throw new Exception("Error deleting item", ex);
+        }
     }
 }
